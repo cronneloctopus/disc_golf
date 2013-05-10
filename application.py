a6040cbbe0ebcc6d1dfe7245482ee06cf480ebaf
Sam Clarke <darwin.galap@gmail.com>
2013-05-09 21:38:59 -0700

    move debug toolbar code to __main__ block

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 from flask.ext.mongoengine import MongoEngine
-#import flask_debugtoolbar
 from config import app
 
 
@@ -21,11 +20,13 @@ db = MongoEngine(app)
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    
+
     app.debug = True
 
+    # add flask debug toolbar
+    import flask_debugtoolbar
+
     # Specify the debug panels you want
-    
     app.config['DEBUG_TB_PANELS'] = [
         'flask_debugtoolbar.panels.versions.VersionDebugPanel',
         'flask_debugtoolbar.panels.timer.TimerDebugPanel',
@@ -39,5 +40,5 @@ if __name__ == '__main__':
         'flask_debugtoolbar_mongo.panel.MongoDebugPanel',
     ]
     toolbar = flask_debugtoolbar.DebugToolbarExtension(app)
-    
+
     app.run(host='0.0.0.0', port=port)
